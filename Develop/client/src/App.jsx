4f94c0b769cc5560ccc7d44bc27c769b20912cd0
Akafifi <itsmeafifi@gmail.com>
2023-11-06 00:16:44 -0600

    app jsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,35 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import Auth from './utils/auth'
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = Auth.getToken()
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+
+})
+
+const slient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
