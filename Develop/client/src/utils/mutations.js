a8b7e89e19c87aa64a0b97ba5a2edab0168fd04f
Akafifi <itsmeafifi@gmail.com>
2023-11-09 21:03:31 -0600

    queries and mutations

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -21,5 +21,70 @@ mutation createUser($user: UserInput) {
         )
     }
 }
+`
+export const LOGIN = gql`
+mutation Login($password: String!,  $username: String, $email: String) {
+    login(password: $password, username: $username, email: $email) {
+        token
+        user (
+            _id
+            email
+            username
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        )
+    }
+}
+`
 
+export const SAVE_BOOK = gql`
+mutation SaveBook($_id: ID!, $book: BookInput!) {
+    saveBook(_id: $_id, book: $book) {
+        token
+        user (
+            _id
+            email
+            username
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        )
+    }
+}
+`
+
+
+export const DELETE_BOOK = gql`
+mutation DeleteBook($user: UserInput) {
+    deleteBook(_id: $_id, bookId: $bookId) {
+        token
+        user (
+            _id
+            email
+            username
+            savedBooks {
+                _id
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        )
+    }
+}
 `
